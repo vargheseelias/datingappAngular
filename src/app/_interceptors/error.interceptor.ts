004900e0e6ba374d8a63722cc09379f3939ac328
vargheseelias <vargheseelias10@gmail.com>
2021-07-22 17:35:25 +0530

    end of section 14

--- src/app/_interceptors/error.interceptor.ts
+++ src/app/_interceptors/error.interceptor.ts
@@ -29,11 +29,13 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modalStateErrors.flat();
+              } else if(typeof(error.error) == 'object') {
+                // this.toastr.error(error.statusText==="ok"?
+                // "unauthorized":error.statusText, error.status);
+                this.toastr.error(error.statusText,error.status);
               } else {
-                this.toastr.error(error.statusText==="ok"?
-                "unauthorized":error.statusText, error.status);
+                this.toastr.error(error.error, error.status);
               }
-
               break;
             case 401:
               this.toastr.error(error.statusText, error.status);
@@ -45,11 +47,11 @@ export class ErrorInterceptor implements HttpInterceptor {
                 const navigationExtras:NavigationExtras={state:{error:error.error}}
                 this.router.navigateByUrl('/serevr-error',navigationExtras)
                 break;
-          
+
             default:
               this.toastr.error('something unexpected went wrong');
               console.log(error);
-              
+
               break;
           }
         }
